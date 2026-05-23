dbd1e58b7bf0d01bef2a02e0328a410132e811fb
HN <Hamza23@seznam.cz>
2026-05-23 13:23:29 +0200

    Poupravil tlacitka, rate limit v backend

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,29 @@
-# main.py
-from fastapi import FastAPI
-import pandas as pd
-import json
 import os
+import json
+import pandas as pd
+from fastapi import FastAPI
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+# Načteme prázdnou paměť a soubor s routou pro nezaměstnanost
 from store import data_cache
 import nezamestnanost 
 
+# 1. Inicializace Limiteru (bude sledovat IP adresy uživatelů)
+# default_limits nastaví globální limit pro VŠECHNY endpointy v aplikaci
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["100/minute"]
+)
+
 app = FastAPI()
+
+# 2. Propojení limiteru s FastAPI a registrace chybového handleru
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+# 3. Tímto propojíme endpoint z nezamestnanost.py s hlavní aplikací
 app.include_router(nezamestnanost.router)
 
 @app.on_event("startup")
